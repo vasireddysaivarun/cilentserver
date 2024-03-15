b29a26f20929a08b50b766f6a457bcdb79291c14
vasireddysaivarun <109501950+vasireddysaivarun@users.noreply.github.com>
2024-03-15 15:22:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const requestListener = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+    if(req.method=== 'POST')    {
+        res.end('Hello World\n Welcome to Node Js with POST');
+      }
+    if(req.method === 'GET'){
+      res.end('Hello World\n Welcome to Node Js with GET');
+   }
+ }
+const server = http.createServer(requestListener);
+server.listen(port, hostname);
